a7ea86461b5de1eb3944cc37d897d5dbebd1a578
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-02-02 13:21:10 +1030

    refactored code

--- number-reversal.c
+++ number-reversal.c
@@ -5,7 +5,7 @@
 int main (void) {
     int i, j, k;
 
-    printf("Enter a two digit number: ");
+    printf("Enter a three digit number: ");
     scanf("%1d%1d%1d", &i, &j, &k);
 
     printf("The reversal is: %d%d%d", k, j, i);
